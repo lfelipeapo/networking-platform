fad5d15b847aea0a47acf9e911c9ba9fad18d23e
Luiz Felipe Apolinário <lfelipeapo@hotmail.com>
2025-11-07 22:29:22 -0300

    chore: suprimir erros de hidratação causados por plugins de navegador

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000, // 25 seconds
+    pagesBufferLength: 2,
+  },
 };
 
 export default nextConfig;
